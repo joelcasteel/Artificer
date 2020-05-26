4c38feb1c58ec0767e7510f08f37c178cfedfaba
Joel Casteel <joel@casteel.info>
2020-05-26 16:38:06 -0700

    Added Basic FileSystem Support.

--- src/main/java/artificer/Main.java
+++ src/main/java/artificer/Main.java
@@ -1,5 +1,11 @@
 package main.java.artificer;
 
+import java.io.File;
+import java.io.FileWriter;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import javafx.application.*;
 import main.java.artificer.ui.App;
 
@@ -7,8 +13,31 @@ import main.java.artificer.ui.App;
 
 public class Main {
     
+    public static String saveDir;
+    
     
 	public static void main(String[] args) {
+	    saveDir = System.getProperty("user.home") +  "\\.artificer\\";
+	    File file = new File(saveDir);
+	    if(!file.exists()) {
+	        file.mkdir();
+	    }
+	    System.out.println(saveDir);
+	    try {
+	        JsonObject src = new JsonObject();
+	        src.addProperty("Hello", "There");
+	        Gson gson = new Gson();
+	        String json = gson.toJson(src);
+	        FileWriter writer = new FileWriter(saveDir + "monsters.json");
+	        writer.write(json);
+	        writer.close();
+	        
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	    
+	    
+	    
 	    //I'm using this as a workaround because fucking eclipse.
 		Application.launch(App.class, args);
 	}
